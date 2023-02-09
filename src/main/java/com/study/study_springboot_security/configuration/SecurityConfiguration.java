8168254115da64934baa2c326d1e41549ee49b60
seonbuzz <ddonsdevelop@gmail.com>
2023-02-09 11:17:46 +0900

    security

---
+++ src/main/java/com/study/study_springboot_security/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.study.study_springboot_security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                // .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/").authenticated() // 로그인 여부만 판단함.
+                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll(); // 설정한 URL 이외에는 접근 가능.
+
+        // 로그인에 대한 부분
+        httpSecurity.formLogin().loginPage("/loginForm")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/");
+        // formLogin 들어가면--> loginPage 지정하기
+
+        return httpSecurity.build();
+    }
+
+}
--- src/main/java/com/study/study_springboot_security/controller/SecurityController.java
+++ src/main/java/com/study/study_springboot_security/controller/SecurityController.java
@@ -6,9 +6,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SecurityController {
-    @GetMapping({ "/admin" })
-    public ModelAndView main(ModelAndView modelAndView) {
-        String viewName = "/WEB-INF/views/admin.jsp";
+
+    @GetMapping({ "/loginForm" }) // 로그인 폼 작성
+    public ModelAndView loginForm(ModelAndView modelAndView) {
+        String viewName = "/WEB-INF/views/security/loginForm.jsp";
+        modelAndView.setViewName(viewName);
+        return modelAndView;
+    }
+
+    @GetMapping({ "/logoutForm" }) // 로그아웃 폼 작성
+    public ModelAndView logoutForm(ModelAndView modelAndView) {
+        String viewName = "/WEB-INF/views/security/logoutForm.jsp";
         modelAndView.setViewName(viewName);
         return modelAndView;
     }
